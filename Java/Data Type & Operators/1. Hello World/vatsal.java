8a0eacbac8aac1b4ed6e69c56898a1aadea65a0a
Vatsal Agrawal <vatsalagrawal1008@gmail.com>
2024-07-31 02:08:03 +0530

    renamed file

--- Java/Data Type & Operators/1. Hello World/vatsal.java
+++ Java/Data Type & Operators/1. Hello World/vatsal.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(string[]args){
-        system.out.println("hello world");
+public class Vatsal {
+    public static void main(String[] args){
+        System.out.println("hello world");
     }
 }
