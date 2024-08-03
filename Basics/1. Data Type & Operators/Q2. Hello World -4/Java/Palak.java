4bf3e0db1e3298e3b510f41401d2a2779b61c1fd
Palak Agarwal <palakarun06@gmail.com>
2024-08-03 20:36:16 +0530

    Solved Hello World problem

---
+++ Basics/1. Data Type & Operators/Q2. Hello World -4/Java/Palak.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Palak {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello 40");
+        System.out.println("50 World !");
+    }
+}
