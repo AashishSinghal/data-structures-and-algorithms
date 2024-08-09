ad7199149f74ff439e54b65ca45111a3b42e32aa
Palak Agarwal <palakarun06@gmail.com>
2024-08-09 21:04:56 +0530

    Solved 10 ques of Data types and operators

---
+++ Basics/1. Data Type & Operators/Q8. A says Hi to B/Java/Palak.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Palak
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String A = scanner.nextLine();
+        String B = scanner.nextLine();
+        System.out.print(A + " says Hi to " + B);
+    }
+}
---
+++ Basics/1. Data Type & Operators/Q9. Print A B C D E/Java/Palak.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Palak
+{
+    public static void main(String[] args)
+    {
+        for(char ch = 'A'; ch <= 'E'; ch++)
+        {
+            System.out.println(ch);
+        }
+    }
+}
